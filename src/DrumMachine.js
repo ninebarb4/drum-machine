c61f8fb644711b062860be6335118ca54762ea32
James Burgess <james.burgess4@gmail.com>
2021-10-28 17:47:18 +0100

    add wrapper basic styling

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './appWrapper.css';
 
 
 export default function DrumMachine() {
     return (
         <div id="drum-machine">
-            WELCOME TO THE DRUM MACHINE
+            
         </div>
     )
 }

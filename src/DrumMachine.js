ef2a9b7b2bf0d7092b59d90281139f089d5c6ca7
James Burgess <james.burgess4@gmail.com>
2021-10-29 11:19:47 +0100

    add DrumPad component with letters

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './appWrapper.css';
+import DrumPad from './DrumPad';
 
 
 export default function DrumMachine() {
     return (
         <div id="drum-machine">
-            
+            <DrumPad />
         </div>
     )
 }
---
+++ src/DrumPad.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function DrumPad() {
+    return (
+        <div id="drum-pad-wrapper">
+            <div className="drum-pad">Q</div>
+            <div className="drum-pad">W</div>
+            <div className="drum-pad">E</div>
+            <div className="drum-pad">A</div>
+            <div className="drum-pad">S</div>
+            <div className="drum-pad">D</div>
+            <div className="drum-pad">Z</div>
+            <div className="drum-pad">X</div>
+            <div className="drum-pad">C</div>
+        </div>
+    )
+}
+
+

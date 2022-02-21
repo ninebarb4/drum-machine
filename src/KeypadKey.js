8dfbf26d1fc2525d96379bf9b5e2d839af48cf89
James Burgess <james.burgess4@gmail.com>
2022-02-21 23:00:17 +0000

    add keydown audio playback

--- src/KeypadKey.js
+++ src/KeypadKey.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 
 export default function KeypadKey(props) {
+    //am I adding event lsitener for each keypadKey compononent?
+    
+    const handleKeydown = (event) => {
+        if(event.keyCode === props.bankElement.keyCode){
+            console.log("you clicked key:", event.keyCode);
+            props.playAudio(props.bankElement.keyTrigger);
+        }
+        
+    }
+
+   useEffect (() => {
+        document.addEventListener('keydown', handleKeydown)
+        //do I need to add event listener clean ups?
+    }, []);
+
   return (
     <button className="drum-pad" id={props.bankElement.id} onClick={() => props.playAudio(props.bankElement.keyTrigger)}>{props.bankElement.keyTrigger}
                 <audio src={props.bankElement.url} className="clip" id={props.bankElement.keyTrigger}></audio>
